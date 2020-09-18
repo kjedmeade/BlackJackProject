3fd7009a85693326f3f515ba50ff42d6089f589a
Kayalyn Edmeade <kjedmeade@gmail.com>
2020-09-18 11:08:58 -0700

    first commit

---
+++ src/com/skilldistillery/common/cards/Deck.java
@@ -0,0 +1,31 @@
+package com.skilldistillery.common.cards;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+	private List<Card> cards;
+
+	public Deck() {
+		cards = new ArrayList<Card>(52);
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				cards.add(new Card(rank, suit));
+			}
+		}
+	}
+
+	public int checkDeckSize() {
+		return cards.size();
+	}
+
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+
+	public Card dealCard() {
+		return cards.remove(0);
+	}
+
+}
